94aedccfaa5ca46a91804fdd0150ca3ef4eabac9
deer <capreolina@protonmail.ch>
2021-04-12 10:36:17 +0000

    Licencing; README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project Gish
+
+An experiment in making
+[gishes](https://oddjobs.codeberg.page/odd-jobs.html#gish) work in pre-BB
+MapleStory.
+
+<https://oddjobs.flarum.cloud/d/11-project-gish/>
+
+## gish-ap-calc
+
+Based on work due to @Cortical.
--- gish-ap-calc/main.ts
+++ gish-ap-calc/main.ts
@@ -1,3 +1,26 @@
+/*
+ * @licstart  The following is the entire license notice for the JavaScript
+ * code in this page.
+ *
+ * This file is part of gish-ap-calc.
+ *
+ * gish-ap-calc is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * gish-ap-calc is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with gish-ap-calc.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice for the JavaScript code in
+ * this page.
+ */
+
 class Stats {
     public constructor(
         public str: number,
